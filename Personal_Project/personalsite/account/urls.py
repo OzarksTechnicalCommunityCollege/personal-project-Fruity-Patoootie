e31e32d97a259325863ce95339c9b942c8e972de
Laura <laura.b.kirkpatrick@gmail.com>
2026-03-03 20:49:37 -0600

    login, registration forms work, but no dashboard

--- Personal_Project/personalsite/account/urls.py
+++ Personal_Project/personalsite/account/urls.py
@@ -13,4 +13,14 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(template_name='account/registration/password_change_form.html'),
+        name='password_change'
+    ),
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(template_name='account/registration/password_change_done.html'),
+        name='password_change_done'
+    ),
 ]
