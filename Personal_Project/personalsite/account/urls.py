9022c32cf361168437c8215f59bb77b1854cfa4d
Laura <laura.b.kirkpatrick@gmail.com>
2026-03-29 12:42:33 -0500

    urls updated, added JS to user/detail.html

--- Personal_Project/personalsite/account/urls.py
+++ Personal_Project/personalsite/account/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+    path('users/<username>/', views.user_detail, name='user_detail'),
     path(
         'password-change/',
         auth_views.PasswordChangeView.as_view(template_name='account/registration/password_change_form.html'),
