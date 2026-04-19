acc4842042528fb91967b3d8b5ecec194b2ca333
Laura <laura.b.kirkpatrick@gmail.com>
2026-04-19 16:32:20 -0500

    Module7

--- Personal_Project/personalsite/mtg/sitemaps.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.sitemaps import Sitemap
-from .models import Card
-class CardSitemap(Sitemap):
-    changefreq = 'weekly'
-    priority = 0.9
-    def items(self):
-        return Card.objects.all()
-    # def lastmod(self,obj):
-    #     return obj.updated
