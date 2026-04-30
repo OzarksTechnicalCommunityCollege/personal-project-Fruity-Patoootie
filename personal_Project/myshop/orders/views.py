2477740c70c37a08ce179fc95e26cdf46268c129
Laura <laura.b.kirkpatrick@gmail.com>
2026-04-30 18:13:09 -0500

    Finished! AND WORKING!!!

--- personal_Project/myshop/orders/views.py
+++ personal_Project/myshop/orders/views.py
@@ -1,5 +1,6 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from cart.cart import Cart
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .forms import OrderCreateForm
 from .models import OrderItem
 from .tasks import order_created
@@ -33,3 +34,9 @@ def order_create(request):
         {'cart': cart, 'form': form}
     )
 
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request, 'admin/orders/order/detail.html', {'order': order}
+    )
--- personal_Project/myshop/payment/webhooks.py
+++ personal_Project/myshop/payment/webhooks.py
@@ -32,5 +32,6 @@ def stripe_webhook(request):
                 return HttpResponse(status=404)
             # mark order as paid
             order.paid = True
+            order.stripe_id = session.payment_intent
             order.save()
     return HttpResponse(status=200)
